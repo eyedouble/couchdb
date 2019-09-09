cac87b554e5170aa2343035194b2c14ac83ba47b
irsanvanwel <irsan@development.local>
2019-09-09 18:24:00 +0200

    Docs Update

--- src/Elixir.CouchDB.Server.erl
+++
@@ -1,7 +0,0 @@
--module('Elixir.CouchDB.Server').
-
--export([
-    info/1
-]).
-
-info(Server) -> couchdb_server:info(Server).
--- src/Elixir.CouchDB.erl
+++
@@ -1,9 +0,0 @@
--module('Elixir.CouchDB').
-
--export([
-    server_record/1
-    ,server_record/2
-]).
-
-server_record(Host) -> couchdb:server_record(Host).
-server_record(Host, Options) -> couchdb:server_record(Host, Options).
